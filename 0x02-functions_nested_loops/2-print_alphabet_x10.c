ed5ebd1fe60d3f08a12c81b21198fb1f899ed490
muhdmaahee2000 <youngzmahi99@gmail.com>
2022-09-14 14:46:21 +0100

    print alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print alphabet in lower case 10x
+ *
+ * Return: Always 0
+ */
+{
+	char ch, i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (ch = 'a'; ch >= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
