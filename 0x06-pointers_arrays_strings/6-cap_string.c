2bcb4d501ab852186011c5d52350aa6a0db026c3
muhdmaahe2000 <you@example.com>
2022-09-22 18:23:36 +0100

    pointers

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,30 +1,31 @@
-#include"main.h"
-i wnta hhhhhhhhhhhh
+#include "main.h"
 /**
  * cap_string - capitalizes all words of a string
- * @s: input string
- * Return: string with capitalized words
- *
- *
+ * @s: input string.
+ * Return: the pointer to dest.
  */
+
+
 char *cap_string(char *s)
 {
-	char *ptr = s;
+int count = 0, i;
+int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	int foundDelimit = 1;
-
-	while (*s)
-	{
-		if (isDelimiter(*s))
-			foundDelimit = 1;
-		else if (isLower(*s) && foundDelimit)
-		{
-			*s -= 32;
-		foundDelimit = 0;
-		}
-		else
-			foundDelimit = 0;
-		s++;
-	}
-	return (ptr);
+if (*(s + count) >= 97 && *(s + count) <= 122)
+*(s + count) = *(s + count) - 32;
+count++;
+while (*(s + count) != '\0')
+{
+for (i = 0; i < 13; i++)
+{
+if (*(s + count) == separators[i])
+{
+if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+*(s + (count + 1)) = *(s + (count + 1)) - 32;
+break;
+}
+}
+count++;
+}
+return (s);
 }
