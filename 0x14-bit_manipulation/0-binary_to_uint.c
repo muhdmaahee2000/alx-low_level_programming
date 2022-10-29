4f321c2a95274d559bc693069221cc37c653a0f6
maahee2000 <yongzmahi99@gmail.com>
2022-10-29 02:41:24 +0100

    #ifndef _MAIN_H

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,73 +1,30 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
-* binary_to_uint - converts a binary number to an unsigned int
-* @b: is pointing to a string of 0 and 1 chars
-* Return: the converted number, or 0 if
-*	there is one or more chars in the string b that is not 0 or 1
-* or b is NULL
-*/
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: pointer to a string containing a binary number
+ *
+ * Return: unsigned int with decimal value of binary number, or 0 if error
+ */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int sum;
-	int power;
+	int i;
+	unsigned int n;
 
-	if (b == NULL)
+	n = 0;
+	if (!b)
 		return (0);
-
-	sum = 0;
-	power = _strlen(b) - 1;
-	for (; *b != '\0'; b++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (*b != 48 && *b != 49)
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-
-		if (*b == '1')
-		{
-			sum += 1 * _pow(2, power);
-			power--;
-		}
-		else
-		{
-			power--;
-		}
 	}
-	return (sum);
-}
-
-/**
-* _strlen - Returns the length of a string
-* @s: pointer to character
-* Return: length of the string 'count'
-*/
-int _strlen(const char *s)
-{
-	int count;
-
-	for (count = 0; *s != '\0'; s++)
-		count++;
-
-	return (count);
-}
-
-/**
-* _pow - compute the value of x raised to the power of y
-* @x: base
-* @y: power
-* Return: returns the value of x raised to the power of y
-*/
-int _pow(int x, int y)
-{
-	if (y == 0)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		return (1);
+		n <<= 1;
+		if (b[i] == '1')
+			n += 1;
 	}
-	else if (y < 0)
-	{
-		return (-1);
-	}
-	y--;
-
-	return (x * _pow(x, y));
+	return (n);
 }
